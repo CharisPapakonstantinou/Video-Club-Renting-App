3b448d3219bf164e5e8e672d0c5cf7fbde15e8ab
Charis Pap <45010931+CharisPapakonstantinou@users.noreply.github.com>
2019-01-30 22:50:48 +0200

    Add files via upload

---
+++ ViewModel/NewCustomerViewmodel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModel
+{
+    public class NewCustomerViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; } // IEnumerable does not provide fuctionality like adding an object, removing an object, accessing an object by index .. We use it when we want only to iterate the object
+        public Customer Customer { get; set; }
+    }
+}
---
+++ ViewModel/NewMovieViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModel
+{
+    public class NewMovieViewModel
+    {
+        public IEnumerable<Genre> Genres { get; set; }
+        // public Movie Movie { get; set; }
+
+        public int? Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int? GenreId { get; set; }
+
+        //[Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        
+        [Range(1, 20)]
+        [Required]
+        public int? NumberInStock { get; set; }
+
+        public string Title
+        {
+            get
+            {
+                return Id != 0 ? "Edit Movie" : "New Movie";
+            }
+        }
+
+        public NewMovieViewModel()
+        {
+            Id = 0;
+        }
+
+        public NewMovieViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            GenreId = movie.GenreId;
+            Name = movie.Name;
+            NumberInStock = movie.NumberInStock;
+            ReleaseDate = movie.ReleaseDate;
+        }
+    }
+}
